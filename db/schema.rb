c33f07c8ad0226f9edd6ff45fe2a2fc15c6d7599
MarSchlaefer <mschlaefer00@gmail.com>
2019-04-26 11:25:51 -0400

    Added name column to game model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_26_144720) do
+ActiveRecord::Schema.define(version: 2019_04_26_152415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2019_04_26_144720) do
     t.integer "sport_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   create_table "sports", force: :cascade do |t|
